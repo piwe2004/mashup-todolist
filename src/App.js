9cea27e613d179abf2fbe5e5fdbcb4492f1a6aa7
piwe2004 <piwe2004@gmail.com>
2021-04-29 16:30:45 +0900

    todolist만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHeade";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
+
+const GlobalStyle = createGlobalStyle
+`
+  body{
+    background:#e9ecef;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+      </TodoTemplate>
+    </>
   );
 }
 
